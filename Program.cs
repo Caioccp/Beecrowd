cd486d62059341ba63a1fe2335a2676f7556b024
caio <caio_pouzada@estudante.sesisenai.org.br>
2026-08-10 20:35:15 -0300

    Questão 1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string[] entradas = Console.ReadLine().Split(' ');
+
+int codigo = int.Parse(entradas[0]);
+int qtde = int.Parse(entradas[1]);
+double total = 0;
+
+if (codigo == 1)
+{
+    total = qtde * 4;
+}
+else if (codigo == 2)
+{
+    total = qtde * 4.50;
+}
+else if (codigo == 3)
+{
+    total = qtde * 5;
+}
+else if (codigo == 4)
+{
+    total = qtde * 2;
+}
+else if (codigo == 5)
+{
+    total = qtde * 1.50;
+}
+
+Console.WriteLine($"Total: R$ {total:F2}");
