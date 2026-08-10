686eca5f573792f1e180bf695c6d498c7811f163
caio <caio_pouzada@estudante.sesisenai.org.br>
2026-08-10 20:56:21 -0300

    Questão 2

--- Program.cs
+++ Program.cs
@@ -1,28 +1,10 @@
-﻿string[] entradas = Console.ReadLine().Split(' ');
-
-int codigo = int.Parse(entradas[0]);
-int qtde = int.Parse(entradas[1]);
-double total = 0;
-
-if (codigo == 1)
-{
-    total = qtde * 4;
-}
-else if (codigo == 2)
-{
-    total = qtde * 4.50;
-}
-else if (codigo == 3)
-{
-    total = qtde * 5;
-}
-else if (codigo == 4)
-{
-    total = qtde * 2;
-}
-else if (codigo == 5)
-{
-    total = qtde * 1.50;
-}
-
-Console.WriteLine($"Total: R$ {total:F2}");
+﻿string[] valores = Console.ReadLine().Split(' ');
+        
+        int A = int.Parse (valores[0]);
+        int B = int.Parse (valores[1]);
+        
+        if (A % B == 0 || B % A == 0) {
+            Console.WriteLine ("Sao Multiplos");
+        } else {
+            Console.WriteLine ("Nao sao Multiplos");
+        }
