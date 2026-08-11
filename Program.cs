15480a2dfd7debb5dc2de0313aa70a897793c398
caio <caio_pouzada@estudante.sesisenai.org.br>
2026-08-10 21:11:16 -0300

    Questão 3

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿string[] valores = Console.ReadLine().Split(' ');
-        
-        int A = int.Parse (valores[0]);
-        int B = int.Parse (valores[1]);
-        
-        if (A % B == 0 || B % A == 0) {
-            Console.WriteLine ("Sao Multiplos");
-        } else {
-            Console.WriteLine ("Nao sao Multiplos");
-        }
+﻿int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int C = int.Parse(Console.ReadLine());
+int D = int.Parse(Console.ReadLine());
+int E = int.Parse(Console.ReadLine());
+int total = 0;
+
+if (A % 2 == 0) {
+    total++;
+}
+if (B % 2 == 0) {
+    total++;
+}
+if (C % 2 == 0) {
+    total++;
+}
+if (D % 2 == 0) {
+    total++;
+}
+if (E % 2 == 0) {
+    total++;
+}
+Console.WriteLine($"{total} valores pares");
