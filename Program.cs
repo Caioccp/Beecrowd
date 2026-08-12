721de5935cd2eb0339c8db1cd9e1f4e9ce44424a
caio <caio_pouzada@estudante.sesisenai.org.br>
2026-08-12 20:44:37 -0300

    Questão 7

--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
-﻿int I = int.Parse(Console.ReadLine());
-char a = 'a';
-string total = "";
+﻿int opcao = 1;
 
-while (I < 1 || I > 10000)
-{
-    I = int.Parse(Console.ReadLine());
+while (opcao == 1) {
+    
+double nota1 = double.Parse(Console.ReadLine());
+double nota2 = double.Parse(Console.ReadLine());
+
+while (nota1 < 0 || nota1 > 10 ) {
+    Console.WriteLine("Nota inválida");
+    nota1 = double.Parse(Console.ReadLine());
 }
 
-total = new string('a', I);
+while (nota2 < 0 || nota2 > 10) {
+    Console.WriteLine("Nota inválida");
+    nota2 = double.Parse(Console.ReadLine());
+}
 
-Console.WriteLine($"Feliz nat{total}l!");
+double media = (nota1 + nota2) / 2; 
+Console.WriteLine($"media = {media}");
 
+Console.WriteLine ("Novo calculo (1-sim 2-nao)");
+ opcao = int.Parse(Console.ReadLine());
+
+while (opcao != 1 && opcao != 2) {
+    opcao = int.Parse (Console.ReadLine());
+}
+}
