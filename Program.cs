2fc85cf13cebc64fcd80fa7484d61fed5362b05c
caio <caio_pouzada@estudante.sesisenai.org.br>
2026-08-13 19:37:52 -0300

    Questão 10

--- Program.cs
+++ Program.cs
@@ -1,11 +1,32 @@
-﻿int[] N = new int[20];
+﻿using System;
 
-for (int i = 0; i < N.Length; i++)
+class URI
 {
-   N[i] =int.Parse(Console.ReadLine());
-}
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] X = new int[N];
 
- for (int i = 0; i < N.Length; i++)
- {
-    Console.WriteLine($"N[{i}] = {N[N.Length - 1 - i]}");
- }
+        string[] entrada = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < N; i++)
+        {
+            X[i] = int.Parse(entrada[i]);
+        }
+
+        int menor = X[0];
+        int posicao = 0;
+
+        for (int i = 1; i < N; i++)
+        {
+            if (X[i] < menor)
+            {
+                menor = X[i];
+                posicao = i;
+            }
+        }
+
+        Console.WriteLine($"Menor valor: {menor}");
+        Console.WriteLine($"Posicao: {posicao}");
+    }
+}
