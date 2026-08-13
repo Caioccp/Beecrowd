d52bf5f468d8c12fc29fc0b248556136a039ed11
caio <caio_pouzada@estudante.sesisenai.org.br>
2026-08-12 21:07:45 -0300

    Questão 8

--- Program.cs
+++ Program.cs
@@ -1,27 +1,56 @@
-﻿int opcao = 1;
+﻿int[] X = new int[10];
 
-while (opcao == 1) {
-    
-double nota1 = double.Parse(Console.ReadLine());
-double nota2 = double.Parse(Console.ReadLine());
-
-while (nota1 < 0 || nota1 > 10 ) {
-    Console.WriteLine("Nota inválida");
-    nota1 = double.Parse(Console.ReadLine());
+for (int i = 0; i < X.Length; i++) {
+    X[i] = int.Parse(Console.ReadLine());
 }
 
-while (nota2 < 0 || nota2 > 10) {
-    Console.WriteLine("Nota inválida");
-    nota2 = double.Parse(Console.ReadLine());
+if (X[0] <= 0) {
+    X[0] = 1;
+}  
+
+if (X[1] <= 0) {
+    X[1] = 1;
 }
 
-double media = (nota1 + nota2) / 2; 
-Console.WriteLine($"media = {media}");
+if (X[2] <= 0) {
+    X[2] = 1;
+} 
 
-Console.WriteLine ("Novo calculo (1-sim 2-nao)");
- opcao = int.Parse(Console.ReadLine());
+if (X[3] <= 0) {
+    X[3] = 1;
+} 
 
-while (opcao != 1 && opcao != 2) {
-    opcao = int.Parse (Console.ReadLine());
+if (X[4] <= 0) {
+    X[4] = 1;
+}  
+
+if (X[5] <= 0) {
+    X[5] = 1;
+} 
+
+if (X[6] <= 0) {
+    X[6] = 1;
 }
-}
+
+if (X[7] <= 0) {
+    X[7] = 1;
+}  
+
+if (X[8] <= 0) {
+    X[8] = 1;
+}  
+
+if (X[9] <= 0) {
+    X[9] = 1;
+} 
+
+Console.WriteLine ($"X[0] = {X[0]}");
+Console.WriteLine ($"X[1] = {X[1]}");
+Console.WriteLine ($"X[2] = {X[2]}");
+Console.WriteLine ($"X[3] = {X[3]}");
+Console.WriteLine ($"X[4] = {X[4]}");
+Console.WriteLine ($"X[5] = {X[5]}");
+Console.WriteLine ($"X[6] = {X[6]}");
+Console.WriteLine ($"X[7] = {X[7]}");
+Console.WriteLine ($"X[8] = {X[8]}");
+Console.WriteLine ($"X[9] = {X[9]}");
